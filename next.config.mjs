e617c493ae950d79f363c7e1a825721e3984051d
Zahid <zahid.imx@gmail.com>
2025-02-28 12:32:05 +0600

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
-// next.config.mjs
 import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
-  pwa: {
-    dest: 'public', // Destination folder for the service worker
-    disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
-  },
+  dest: 'public', // Destination folder for the service worker
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development mode
 });
 
 export default nextConfig;
